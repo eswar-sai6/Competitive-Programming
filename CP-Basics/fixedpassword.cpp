387c6a72a9d315bf433c5f3b3606bc7c2f59ad1b
Bendi Eswar Sai <bendieswarsai@gmail.com>
2023-11-09 18:17:12 +0530

    a new cpp file called fixedpassword is added

---
+++ CP-Basics/fixedpassword.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+long long k;
+long long n=INT_MAX;
+for(long long i=1;i<=n;i++)
+{
+cin>>k;
+if(k!=1999){
+cout<<"Wrong";
+}
+if(k==1999){
+cout<<"Correct";
+break;
+}
+}
+}
